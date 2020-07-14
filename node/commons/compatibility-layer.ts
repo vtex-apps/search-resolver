7e303d6c32a1951e6de474d015f92c0de7c7211a
lucastornai <lucas@tornai.dev>
2020-07-14 16:18:28 -0300

    Fix specification groups not being parsed beforehand

--- node/commons/compatibility-layer.ts
+++ node/commons/compatibility-layer.ts
@@ -37,7 +37,9 @@ export const convertBiggyProduct = (
 
   const allSpecifications = product.productSpecifications.concat(getSKUSpecifications(product))
 
-  const allSpecificationsGroups = [ ...product.specificationGroups.keys() ]
+  const specificationGroups = JSON.parse(product.specificationGroups)
+
+  const allSpecificationsGroups = [ ...Object.keys(specificationGroups) ]
 
   const brandId = product.brandId ? Number(product.brandId) : -1
 
@@ -98,7 +100,7 @@ export const convertBiggyProduct = (
   })
 
   allSpecificationsGroups.forEach((specificationGroup) => {
-    convertedProduct[specificationGroup] = product.specificationGroups.get(specificationGroup)
+    convertedProduct[specificationGroup] = specificationGroups[specificationGroup]
   })
 
   return convertedProduct
