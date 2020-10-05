c435cd2cb3670ee6b1ef0683dea547696004b8df
iaron <iaron.araujo@vtex.com.br>
2020-10-05 11:34:46 -0300

    Added typings

--- node/resolvers/search/facets.ts
+++ node/resolvers/search/facets.ts
@@ -1,10 +1,10 @@
 export const resolvers = {
   Facet: {
-    values: (facet: any, args: any) => {
+    values: (facet: Facet, args: FacetValuesArgs) => {
       const { values } = facet
       return values.slice(args.from ?? 0, args.to)
     },
-    quantity: (facet: any) => {
+    quantity: (facet: Facet) => {
       return facet.values.length ?? 0
     }
   },
