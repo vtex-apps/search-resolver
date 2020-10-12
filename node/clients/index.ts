18ab3036d005150e3a244ed52b0b8c8a48e6b9b3
Victor Raposo <vwraposo@gmail.com>
2020-10-12 14:14:41 -0300

    Adds catalog client to get group name/id map

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -3,6 +3,7 @@ import { IOClients } from '@vtex/api'
 import { Search } from './search'
 import { Checkout } from './checkout'
 import { Rewriter } from './rewriter'
+import { Catalog } from './catalog'
 
 export class Clients extends IOClients {
   public get search() {
@@ -13,5 +14,8 @@ export class Clients extends IOClients {
   }
   public get rewriter() {
     return this.getOrSet('rewriter', Rewriter)
+  }
+  public get catalog() {
+    return this.getOrSet('catalog', Catalog)
   }
 }
