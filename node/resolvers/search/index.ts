53056f9a17be53c7d202bfec4cb23670b3e76e20
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2021-02-11 13:31:56 -0300

    add validation on the filter

--- node/resolvers/search/index.ts
+++ node/resolvers/search/index.ts
@@ -451,6 +451,7 @@ export const queries = {
       attributes: attributesWithVisibilitySet,
       selectedFacets: args.selectedFacets,
       removeHiddenFacets: args.removeHiddenFacets,
+      showCategoryTree: args.showCategoryTree,
     })
 
     return {
