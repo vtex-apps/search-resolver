51e661b1526db241f0cf6304abe732e8d27dec55
igorfratel <igorfratelsantana@gmail.com>
2021-03-29 14:31:35 -0300

    restore timemout configs

--- node/index.ts
+++ node/index.ts
@@ -48,6 +48,7 @@ export default new Service<Clients, RecorderState, CustomContext>({
       },
       biggySearch: {
         concurrency: 10,
+        timeout: SIX_SECONDS_MS,
       },
       vbase: {
         concurrency: 2,
