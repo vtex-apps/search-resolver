1a65f96ac8ca795a22088adc341306ecd95a86a5
Christian Mutti <christian.mutti@vtex.com.br>
2021-04-20 12:55:37 -0300

    Add complementName to BiggySKU and conversion

--- node/typings/Search.ts
+++ node/typings/Search.ts
@@ -57,7 +57,7 @@ interface SuggestionProductsArgs {
   productOriginVtex: boolean
   simulationBehavior: 'skip' | 'default' | null
   sellers?: RegionSeller[]
-  hideUnavailableItems?: boolean | null,
+  hideUnavailableItems?: boolean | null
   regionId?: string
 }
 
@@ -173,6 +173,7 @@ interface BiggyProductExtraData {
 interface BiggySearchSKU {
   name: string
   nameComplete: string
+  complementName?: string
   id: string
   ean?: string
   reference: string
