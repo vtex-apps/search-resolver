f6d7b6a5059bad33986655fe12910de3e17184cf
Christian Mutti <christian.mutti@vtex.com.br>
2021-06-17 15:36:29 -0300

    Revert "Revert "Remove regionalizationV2""
    This reverts commit 5a32756d10a565a8453e12eb878c39fda7f8c8e0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Removed
+- `regionalizationv2` query string.
+
 ## [1.46.0] - 2021-06-17
 
 ### Added
--- node/clients/biggy-search.ts
+++ node/clients/biggy-search.ts
@@ -156,7 +156,6 @@ export class BiggySearchClient extends ExternalClient {
         params: {
           locale: this.locale,
           ['hide-unavailable-items']: hideUnavailableItems ?? false,
-          regionalizationv2: true,
         },
         headers: {
           Cookie: buildBSearchFilterCookie(sellers),
@@ -235,7 +234,6 @@ export class BiggySearchClient extends ExternalClient {
         operator,
         fuzzy,
         locale: this.locale,
-        regionalizationv2: true,
         bgy_leap: leap ? true : undefined,
         ['hide-unavailable-items']: hideUnavailableItems ? 'true' : 'false',
         ...parseState(searchState),
@@ -280,7 +278,6 @@ export class BiggySearchClient extends ExternalClient {
       fuzzy,
       locale: this.locale,
       bgy_leap: leap ? true : undefined,
-      regionalizationv2: true,
       allowRedirect: options?.allowRedirect === false ? false : true,
       ['hide-unavailable-items']: hideUnavailableItems ? 'true' : 'false',
       ...parseState(searchState),
