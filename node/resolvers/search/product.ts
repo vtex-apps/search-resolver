799f6a4f17b68c05c5f92476957301e0ce7a9bb2
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2021-09-17 14:56:16 -0300

    Revert "Merge pull request #221 from vtex-apps/feature/vtexis-compatibility-layer"
    This reverts commit 5a27e3f196cde9a37812914b582007e5f8cc43b0, reversing
    changes made to aa413ea61224d704d29f115c8075420d3595c6d4.

--- node/resolvers/search/product.ts
+++ node/resolvers/search/product.ts
@@ -170,14 +170,8 @@ export const resolvers = {
     cacheId: ({ linkText, cacheId }: SearchProductWithCache) =>
       cacheId || linkText,
 
-    clusterHighlights: ({origin, clusterHighlights = {} }: SearchProduct) => {
-      if (origin === 'intelligent-search') {
-        return clusterHighlights
-      }
-
-
-      return objToNameValue('id', 'name', clusterHighlights)
-    },
+    clusterHighlights: ({ clusterHighlights = {} }: SearchProduct) =>
+      objToNameValue('id', 'name', clusterHighlights),
 
     jsonSpecifications: (product: SearchProduct) => {
       const { Specifications = [] } = product
@@ -191,13 +185,8 @@ export const resolvers = {
       return JSON.stringify(specificationsMap)
     },
 
-    productClusters: ({ origin, productClusters = {} }: SearchProduct) => {
-      if (origin === 'intelligent-search') {
-        return productClusters
-      }
-
-      return objToNameValue('id', 'name', productClusters)
-    },
+    productClusters: ({ productClusters = {} }: SearchProduct) =>
+      objToNameValue('id', 'name', productClusters),
 
     properties: async (product: SearchProduct, _: unknown, ctx: Context) => {
       const valuesUntranslated = (product.allSpecifications ?? []).map((name: string) => {
