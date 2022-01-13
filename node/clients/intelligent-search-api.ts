61e02639e7dce31d339725d8db030a42f9cecb20
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2022-01-13 17:41:26 -0300

    migrate facets

--- node/clients/intelligent-search-api.ts
+++ node/clients/intelligent-search-api.ts
@@ -1,4 +1,5 @@
 import { ExternalClient, InstanceOptions, IOContext } from "@vtex/api";
+import { parseState } from "../utils/searchState";
 
 interface CorrectionParams {
   query: string
@@ -16,6 +17,31 @@ interface BannersArgs {
   query: string
 }
 
+interface FacetsArgs {
+  query?: string
+  page?: number
+  count?: number
+  sort?: string
+  operator?: string
+  fuzzy?: string
+  leap?: boolean
+  tradePolicy?: number
+  searchState?: string
+  hideUnavailableItems?: boolean | null
+  removeHiddenFacets?: boolean | null
+  options?: Options
+  initialAttributes?: string
+  workspaceSearchParams?: object
+  regionId?: string | null
+}
+
+const decodeQuery = (query: string) => {
+  try {
+    return decodeURIComponent(query)
+  } catch (e) {
+    return query
+  }
+}
 
 export class IntelligentSearchApi extends ExternalClient {
   private locale: string | undefined
@@ -37,22 +63,35 @@ export class IntelligentSearchApi extends ExternalClient {
   public async topSearches() {
     return this.http.get('/top_searches', {params: {
       locale: this.locale
-    }})
+    }, metric: 'topSearches'})
   }
 
   public async correction(params: CorrectionParams) {
-    return this.http.get('/correction_search', {params: {...params, locale: this.locale}})
+    return this.http.get('/correction_search', {params: {...params, locale: this.locale}, metric: 'correction'})
   }
 
   public async searchSuggestions(params: SearchSuggestionsParams) {
-    return this.http.get('/search_suggestions', {params: {...params, locale: this.locale}})
+    return this.http.get('/search_suggestions', {params: {...params, locale: this.locale}, metric: 'searchSuggestions'})
   }
 
   public async autocompleteSearchSuggestions(params: AutocompleteSearchSuggestionsParams) {
-    return this.http.get('/autocomplete_suggestions', {params: {...params, locale: this.locale}})
+    return this.http.get('/autocomplete_suggestions', {params: {...params, locale: this.locale}, metric: 'autocompleteSearchSuggestions'})
   }
 
   public async banners(params: BannersArgs, path: string) {
-    return this.http.get(`/banners/${path}`, {params: {query: params.query, locale: this.locale}})
+    return this.http.get(`/banners/${path}`, {params: {query: params.query, locale: this.locale}, metric: 'banners'})
+  }
+
+  public async facets(params: FacetsArgs, path: string) {
+    const {query, leap, searchState} = params
+    return this.http.get(`/facets/${path}`, {
+      params: {
+        query: query && decodeQuery(query),
+        locale: this.locale,
+        bgy_leap: leap ? true : undefined,
+        ...parseState(searchState),
+      },
+      metric: 'facets'
+    })
   }
 }
